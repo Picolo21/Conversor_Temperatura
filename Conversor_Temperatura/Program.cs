e3ad30dd86835231bc037c78572b43cb0dbd5f2e
Vinicius Picolo <picolo.vininicius@gmail.com>
2023-03-05 23:38:52 -0300

    Add project files.

---
+++ Conversor_Temperatura/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double C;
+        double F;
+
+        Console.Write("Digite uma temperatura em °C para converter para °F: ");
+        C = double.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        F = ((C * 9) / 5) + 32;
+
+        Console.WriteLine("A temperatura " + C.ToString("F2") + "°C é igual a " + F.ToString("F2") + "°F");
+    }
+}
